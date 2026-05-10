b690086155a770d369c70b3c60dd706dabecfe46
qingyvz <24302010047@m.fudan.edu.cn>
2026-05-10 12:02:02 +0800

    新增gatewaydelopy

--- WisePenView/src/services/Search/SearchServices.impl.ts
+++ WisePenView/src/services/Search/SearchServices.impl.ts
@@ -4,7 +4,7 @@ import type { ISearchService, SearchQueryReqDTO, SearchResultResDTO } from './in
 class SearchServiceImpl implements ISearchService {
     async globalSearch(data: SearchQueryReqDTO): Promise<SearchResultResDTO> {
         // 依据你的确认，暂时不加额外前缀。如后端实际路径有变（如 /search/globalSearch），只需在此处调整即可
-        const res = await Axios.post<SearchResultResDTO>('/globalSearch', data);
+        const res = await Axios.post<SearchResultResDTO>('/search/global', data);
         return res.data;
     }
 }
